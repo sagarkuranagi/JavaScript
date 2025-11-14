1547982b7e6b53bc9b7bd1c545c00d404f6163a1
Mr_sagar_521 <154483781+sagarkuranagi@users.noreply.github.com>
2025-11-14 10:42:17 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,11 @@
+console.log("hello world");
+// rep - red evaluate print loop
+ 
+/*
+1.create anew flie -name.js
+2.wirte code to log your name
+3.save the file
+4.run the flie
+ */
+
+console.log("Kill me the person")
