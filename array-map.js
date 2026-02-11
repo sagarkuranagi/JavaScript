7b80e4d5999878174d4de2a68449d2a46b183b23
Mr Sagar <154483781+sagarkuranagi@users.noreply.github.com>
2026-02-11 15:21:59 +0530

    Add files via upload

---
+++ array-map.js
@@ -0,0 +1,18 @@
+// const arr = [10,11,12,13,14]
+// const result = arr.map(function(ele){
+//     return ele + 5;
+// })
+// console.log(result);
+
+// //multiplaction by 2 using map
+// const number = [1, 2, 3, 4, 5];
+// const result = number.map(function(ele){
+//     return ele * 2;
+// })
+// console.log(result)
+
+const names =["shiva", "sagar", "siri"];
+const result = names.map(function(ele){
+    return ele.toUpperCase();
+})
+console.log(result);
